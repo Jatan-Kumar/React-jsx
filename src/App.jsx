a9f726654389576756a56e104cef4ab52bf9877b
Jatan-Kumar <jatan-c.k@hotmail.com>
2024-05-07 19:55:51 +0100

    Covered useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+//Hooks
+import { useState } from "react";
+
 //Components
 import Banner from "./components/Banner/Banner.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
-import TabButton from "./components/Button/TabButton.jsx"
+import TabButton from "./components/Button/TabButton.jsx";
 
 //Dummy Data
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 
 function App() {
+
+  const [activeTab, setActiveTab] = useState('components');
+
+  function handleSelect(tabName) {
+    setActiveTab(tabName);
+  }
+  
   return (
     <div>
       <Banner />
@@ -14,22 +24,33 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((data) => (
-              <CoreConcepts
-                title={data.title}
-                description={data.description}
-                image={data.image}
-              />
-            ))}
+            {CORE_CONCEPTS.map((data, index) => {
+              return (
+                <CoreConcepts
+                  key={index}
+                  title={data.title}
+                  description={data.description}
+                  image={data.image}
+                />
+              );
+            })}
           </ul>
         </section>
-        <section id='examples'>
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-              <TabButton>
-                testing 
-              </TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}> JSX </TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[activeTab].title}</h3>
+            <p>{EXAMPLES[activeTab].description}</p>
+            <pre>
+              <code>{EXAMPLES[activeTab].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
--- src/components/Button/TabButton.jsx
+++ src/components/Button/TabButton.jsx
@@ -1,4 +1,7 @@
-export default function TabButton (props){
-    console.log({props}, 'props in comp');
- return <li><button>{props.children}</button></li>
+export default function TabButton({children, onSelect}) {
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
 }
